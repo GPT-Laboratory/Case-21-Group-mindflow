d994619d6fb83c224181bd31eb096027b3b9453f
Jaakko Rajala <jaakko.rajala@tuni.fi>
2025-06-04 20:25:09 +0300

    remove unused variable

--- src/AgenticContentFlow/Nodes/common/NodeStyles.tsx
+++ src/AgenticContentFlow/Nodes/common/NodeStyles.tsx
@@ -100,7 +100,6 @@ export function BaseNodeContainer({
   };
 
   const finalStyle = getProcessingStyles();
-  const hasProcessingEffect = processState !== 'idle' || processing;
 
   return (
     <div 
