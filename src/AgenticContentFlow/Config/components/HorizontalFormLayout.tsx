9457d0e9ffcc870b7c2d6ce4a872bc45f0e07369
Jaakko Rajala <jaakko.rajala@tuni.fi>
2025-06-05 21:02:59 +0300

    fix: update FormField import path

--- src/AgenticContentFlow/Config/components/HorizontalFormLayout.tsx
+++ src/AgenticContentFlow/Config/components/HorizontalFormLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormField } from './FormField';
+import { FormField } from './common/FormField';
 
 interface HorizontalFormLayoutProps {
   fields: Record<string, any>;
