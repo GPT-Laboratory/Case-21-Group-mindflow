8f54a0010b893e462985cf11fdd6d72fcf632c9c
Jaakko Rajala <jaakko.rajala@tuni.fi>
2025-06-05 21:02:04 +0300

    remove: deprecated ProcessProvider file

--- src/AgenticContentFlow/Process/ProcessProvider.tsx
+++
@@ -1,5 +0,0 @@
-// Re-export the new ProcessProvider and hook for compatibility
-export { ProcessProvider as default, useProcessContext } from './ProcessContext';
-
-// Legacy compatibility - this file now just re-exports the new ProcessContext
-// All existing imports will continue to work
