12feb0323d4a2672d6ab18e48daf98a886c6af27
Jaakko Rajala <jaakko.rajala@tuni.fi>
2025-06-30 20:53:43 +0300

    feat(node-hooks): implement useNodeColorSync hook

---
+++ src/AgenticContentFlow/Node/hooks/useNodeColorSync.ts
@@ -0,0 +1,49 @@
+import { useCallback, useEffect } from 'react';
+import { useReactFlow, Node } from '@xyflow/react';
+import { UnifiedStyleManager } from '../factory/utils/UnifiedStyleManager';
+import { UnifiedFrameJSON } from '../factory/types/UnifiedFrameJSON';
+
+interface UseNodeColorSyncProps {
+  id: string;
+  config: UnifiedFrameJSON;
+  data: any;
+  styleConfig: any;
+}
+
+export const useNodeColorSync = ({ id, config, data, styleConfig }: UseNodeColorSyncProps) => {
+  const { setNodes } = useReactFlow();
+
+  const updateNodeColors = useCallback(() => {
+    const baseColor = styleConfig.backgroundColor;
+    const minimapColor = UnifiedStyleManager.calculateMinimapColor(config, styleConfig);
+    const handleColor = UnifiedStyleManager.calculateHandleColor(config, styleConfig);
+    
+    // Check if any colors have changed
+    const colorsChanged = 
+      data?.nodeColor !== baseColor ||
+      data?.minimapColor !== minimapColor ||
+      data?.handleColor !== handleColor;
+    
+    if (colorsChanged) {
+      setNodes((nodes: Node[]) =>
+        nodes.map((node: Node) =>
+          node.id === id
+            ? {
+                ...node,
+                data: {
+                  ...node.data,
+                  nodeColor: baseColor,
+                }
+              }
+            : node
+        )
+      );
+    }
+  }, [styleConfig, config, data?.nodeColor, data?.minimapColor, data?.handleColor, id, setNodes]);
+
+  useEffect(() => {
+    updateNodeColors();
+  }, [updateNodeColors]);
+
+  return { updateNodeColors };
+}; 
